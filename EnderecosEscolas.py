23b05dc111c43882b383fe8a1b17e7e0e8f224f6
Reinaldo de Oliveira Maciel <maciel.oliveira.reinaldo@gmail.com>
2018-12-27 15:16:13 -0200

    Add files via upload
    EnderecosEscolas.py, dada uma lista de escolas em formato .csv com cabeçalho CO_ENTIDADE;ESCOLA;CIDADE;UF, sendo CO_ENTIDADE o código INEP da escola, este script retorna o endereço existente no maps para tal escola, bem como as coordenadas geográficas do mesmo.

---
+++ EnderecosEscolas.py
@@ -0,0 +1,49 @@
+import requests
+import os
+import csv
+
+os.chdir('C:/Users/rmaciel/Desktop/python')
+GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
+
+
+def buscaCoordenadas(local):
+	params = {
+	    'address': local,
+	    'sensor': 'false',
+	    'region': 'br',
+	    'key' : 'AIzaSyBcOTfNswUNCq_2qGPxHwwjcV3ddKjC5Bc'
+	}
+	fl = []
+
+	# Do the request and get the response data
+	req = requests.get(GOOGLE_MAPS_API_URL, params=params)
+	res = req.json()
+
+	# Use the first result
+	try:
+		result = res['results'][0]
+		geodata = dict()
+		geodata['lat'] = result['geometry']['location']['lat']
+		geodata['lng'] = result['geometry']['location']['lng']
+		geodata['address'] = result['formatted_address']
+		fl.append(geodata['address'])
+		fl.append(geodata['lat'])
+		fl.append(geodata['lng'])
+	except IndexError:
+		fl.append('não encontrado')
+		fl.append(-15.7750837)
+		fl.append(-48.0772769)
+	finally:
+		return fl
+	
+##abre o arquivo de escolas
+with open ('Escolas3.csv', 'r', encoding='cp1252') as csvfile:
+	baseEscolas = csv.reader(csvfile, delimiter = ';')
+	for row in baseEscolas:
+		busca = row[1] + ', '+row[2]+' - '+row[3]
+		resultado = buscaCoordenadas(busca)
+		arq = open('Escolas6.csv','a', encoding = 'UTF-'8)
+		txt = row[0]+';'+row[1]+';'+row[2]+';'+row[3]+';'+str(resultado[1])+';'+str(resultado[2])+';'+resultado[0]+'\n'
+		print(txt)
+		arq.write(txt)
+		arq.close()
